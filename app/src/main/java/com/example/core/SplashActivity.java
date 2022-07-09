f761a7230bb4bd145b406e009daa1caaef1c68a4
Palash <palashchiplunkar51@gmail.com>
2022-07-09 21:23:26 +0530

    added splash screen

--- app/src/main/java/com/example/core/SplashActivity.java
+++ app/src/main/java/com/example/core/SplashActivity.java
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.window.SplashScreen;
 
-public class splashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override public void run() {
-                Intent i = new Intent(splashScreen.this, MainActivity.class); startActivity(i);
-                finish(); } }, 3000);
+                Intent i = new Intent(SplashActivity.this, loginActivity.class); startActivity(i);
+                finish(); } }, 1000);
     }
-    }
+}
--- app/src/main/java/com/example/core/loginActivity.java
+++ app/src/main/java/com/example/core/loginActivity.java
@@ -28,8 +28,6 @@ public class loginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         loadingPB=findViewById(R.id.PBLoading);
         register=findViewById(R.id.register_btn);
